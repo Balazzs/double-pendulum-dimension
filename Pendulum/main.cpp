29b0a24ed1e15d7a799a2ccde2f9debc625859b6
Balazzs <bogye.balazs@gmail.com>
2018-10-06 19:16:10 +0200

    Refactor

--- Pendulum/main.cpp
+++ Pendulum/main.cpp
@@ -20,6 +20,7 @@ constexpr int numberOfStateVariables = 5;
 constexpr int byteSizeOfData = numberOfStateVariables * dataWidth * dataWidth * sizeof (number);
 
 constexpr int workersPerSide = workSize * globalSizeMultiplier;
+constexpr int numberOfAllWorkers = workersPerSide * workersPerSide / 2;
 
 constexpr int simulationTime = 1;
 constexpr int stepsPerSecond = 1024;
@@ -27,99 +28,95 @@ constexpr int stepsPerKernel = 64;
 constexpr int CPUStepsPerSecond = stepsPerSecond / stepsPerKernel;
 constexpr number timeStep = 1.0 / stepsPerSecond;
 
+std::vector<cl::Event> loadParamsToGPU (const cl::CommandQueue& queue, const cl::Kernel& setupKernel, cl::Buffer& statesBuffer)
+{
+	_setArgs (setupKernel, statesBuffer, dataWidth);
+	
+	std::vector<cl::Event> events (numberOfAllWorkers);
+
+	for (int x = 0; x < workersPerSide / 2; x++)
+		for (int y = 0; y < workersPerSide; y++)
+			queue.enqueueNDRangeKernel (setupKernel, cl::NDRange (x*workSize, y*workSize), cl::NDRange (workSize, workSize), cl::NDRange (workSize, workSize), NULL, &events[x * workersPerSide + y]);
+
+	return events;
+}
+
+void saveDataToFile (const std::vector<number>& data, std::string filename = "out.dat")
+{
+	std::ofstream file (filename, std::ios::out);
+
+	for (int i = 0; i < dataWidth; i++) {
+		for (int j = 0; j < dataWidth; j++)
+			file << data[(i*dataWidth + j) * numberOfStateVariables] << "\t";
+		file << std::endl;
+	}
+
+	file.close ();
+}
+
 int main(int argc, char** args)
 {
 	std::vector<number> states_vec(numberOfStateVariables * dataWidth * dataWidth);
 
 	try{
 		//OpenCL cuccok inicializálása
-		CLO jani = CLO::fromFile("prog.cl");
+		CLO clObject = CLO::fromFile ("prog.cl");
 	
-		auto vec = jani.getKernels({"prog", "load"});
-		auto stuff = jani.getStuff();
+		auto kernels = clObject.getKernels ({"prog", "load"});
 	
-		const cl::CommandQueue& queue = stuff[0].second;
-		const cl::Kernel& ker = vec[0];
-		const cl::Kernel& ker2 = vec[1];
-		const auto& context = jani.getContext();
+		const cl::CommandQueue& queue = clObject.getStuff ()[0].second;
+		const cl::Kernel& simulationKernel = kernels[0];
+		const cl::Kernel& setupKernel = kernels[1];
+		const auto& context = clObject.getContext ();
 
-
-		auto start = std::chrono::high_resolution_clock::now();
+		auto startTime = std::chrono::high_resolution_clock::now ();
 
 		//Bufferek
-		cl::Buffer states(context, CL_MEM_READ_WRITE, byteSizeOfData);
+		cl::Buffer statesBuffer (context, CL_MEM_READ_WRITE, byteSizeOfData);
 	
-
 		//Kernel argumentumok beállítása
-		_setArgs(ker, states, dataWidth, timeStep, stepsPerKernel);
-		_setArgs(ker2, states, dataWidth);
-
-
-		std::vector<cl::Event> prev;
-		cl::Event ev[workSize*workSize*globalSizeMultiplier*globalSizeMultiplier];
-
-		//Load the params
-		for (int x = 0; x < workersPerSide / 2; x++)
-			for (int y = 0; y < workersPerSide; y++)
-				queue.enqueueNDRangeKernel (ker2, cl::NDRange (x*workSize, y*workSize), cl::NDRange (workSize, workSize), cl::NDRange (workSize, workSize), NULL, &ev[x * workersPerSide + y]);
-
-
-		for (int x = 0; x < workersPerSide / 2; x++)
-			for (int y = 0; y < workersPerSide; y++)
-				prev.push_back (ev[x * workersPerSide + y]);
-
-		for (int seconds = 0; seconds < simulationTime; seconds++) 
+		_setArgs (simulationKernel, statesBuffer, dataWidth, timeStep, stepsPerKernel);
+		
+		std::vector<cl::Event> previousEvents;
+		previousEvents = loadParamsToGPU (queue, setupKernel, statesBuffer);
+		
+		for (int time = 0; time < simulationTime; time++) 
 		{
-			std::cout << seconds << std::endl;
+			std::cout << time << std::endl;
 
 			for (int i = 0; i < CPUStepsPerSecond; i++)
 			{
-				//Vége a számolásnak event
-				cl::Event ev[workSize*workSize*globalSizeMultiplier*globalSizeMultiplier];
-				//Számol
+				std::vector<cl::Event> currentEvents (numberOfAllWorkers);
+				
 				for (int x = 0; x < workersPerSide / 2; x++)
 					for (int y = 0; y < workersPerSide; y++)
-					queue.enqueueNDRangeKernel(ker, cl::NDRange(x*workSize, y*workSize), cl::NDRange(workSize, workSize), cl::NDRange(workSize, workSize), &prev, &ev[x * workersPerSide + y]);
+					queue.enqueueNDRangeKernel (simulationKernel, cl::NDRange(x*workSize, y*workSize), cl::NDRange(workSize, workSize), cl::NDRange(workSize, workSize), &previousEvents, &currentEvents[x * workersPerSide + y]);
 
-				//Következő feltételek vektorba
-				prev.clear();
-				for (int x = 0; x < workersPerSide / 2; x++)
-					for (int y = 0; y < workersPerSide; y++)
-						prev.push_back(ev[x * workersPerSide + y]);
-				std::this_thread::sleep_for(std::chrono::microseconds(10));
+				previousEvents = std::move (currentEvents);
+				//Let the GPU do something else for a short time (like draw windows..)
+				std::this_thread::sleep_for (std::chrono::microseconds(10));
 			}
 		}
-		//És kiolvas amint vége a számolásnak
-		auto ev_vec = prev;
-		queue.enqueueReadBuffer(states, CL_TRUE, 0, byteSizeOfData, &states_vec[0], &ev_vec);
 
-		//Nézzük meg az egész GPU-s dolog mennyi CPU oldali időbe telt (ne rondítsunk bele a std out streambe, errorba inkább...)
-		std::cerr << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << " us" << std::endl;
+		//Read out the results
+		queue.enqueueReadBuffer (statesBuffer, CL_TRUE, 0, byteSizeOfData, &states_vec[0], &previousEvents);
 
-		std::ofstream file("out.dat", std::ios::out);
+		std::cout << std::chrono::duration_cast<std::chrono::seconds> (std::chrono::high_resolution_clock::now() - startTime).count () << " s" << std::endl;
 
-		//Írjuk ki az eredményt
-		for (int i = 0; i < dataWidth; i++) {
-			for (int j = 0; j < dataWidth; j++)
-				file << states_vec[(i*dataWidth + j) * numberOfStateVariables] << "\t";
-			file << std::endl;
-		}
-
-		file.close();
+		saveDataToFile (states_vec);
 	}
-	//Ha valami gond lenne
 	catch(cl::Error err)
 	{
 		std::cerr
 		 << "ERROR in main:\n"
-         << err.what()
-         << "(" << err.err() << ")\n"
-		 << error_map[err.err()]
+         << err.what ()
+         << "(" << err.err () << ")\n"
+		 << error_map[err.err ()]
          << std::endl;
 	}
 	catch(std::runtime_error err)
 	{
-		std::cerr << "Runtime error:\n" << err.what() << std::endl;
+		std::cerr << "Runtime error:\n" << err.what () << std::endl;
 	}
 	catch(std::exception e)
 	{
