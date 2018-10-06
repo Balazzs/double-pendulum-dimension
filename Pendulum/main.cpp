071d272d08f63de59d593b1a77fc13149b9926da
Balazzs <bogye.balazs@gmail.com>
2018-10-06 18:46:55 +0200

    Simulation speed-up, increased readability
    -Calculates only half of the plot (the other half is redundant)
    -Temporary save removed since it needed GPU-CPU synchronization
    -Sleep decreased to 10 microseconds (instead of 10ms per 50 steps)
    -Global multiplier introduced for worker numbers since we i can't have more than 32
    -XY grid instead of X slices for kernels
    -typedef for used floating point type. Set to float
    -Almost everything renamed for readability

--- Pendulum/main.cpp
+++ Pendulum/main.cpp
@@ -9,103 +9,103 @@
 #include <limits>
 #include <chrono>
 #include <thread>
+#include <iomanip>
+
+typedef cl_float number;
+
+constexpr int globalSizeMultiplier = 4;
+constexpr int workSize = 8;
+constexpr int dataWidth = workSize * workSize * globalSizeMultiplier;
+constexpr int numberOfStateVariables = 5;
+constexpr int byteSizeOfData = numberOfStateVariables * dataWidth * dataWidth * sizeof (number);
+
+constexpr int workersPerSide = workSize * globalSizeMultiplier;
+
+constexpr int simulationTime = 1;
+constexpr int stepsPerSecond = 1024;
+constexpr int stepsPerKernel = 64;
+constexpr int CPUStepsPerSecond = stepsPerSecond / stepsPerKernel;
+constexpr number timeStep = 1.0 / stepsPerSecond;
 
 int main(int argc, char** args)
 {
-	const int W = 30, N = W*W, NN = 5;
-
-	std::vector<cl_double> states_vec(NN * N * N);
+	std::vector<number> states_vec(numberOfStateVariables * dataWidth * dataWidth);
 
 	try{
-	//OpenCL cuccok inicializálása
-	CLO jani = CLO::fromFile("prog.cl", false);
+		//OpenCL cuccok inicializálása
+		CLO jani = CLO::fromFile("prog.cl");
 	
-	auto vec = jani.getKernels({"prog", "load"});
-	auto stuff = jani.getStuff();
+		auto vec = jani.getKernels({"prog", "load"});
+		auto stuff = jani.getStuff();
 	
-	const cl::CommandQueue& queue = stuff[0].second;
-	const cl::Kernel& ker = vec[0];
-	const cl::Kernel& ker2 = vec[1];
-	const auto& context = jani.getContext();
+		const cl::CommandQueue& queue = stuff[0].second;
+		const cl::Kernel& ker = vec[0];
+		const cl::Kernel& ker2 = vec[1];
+		const auto& context = jani.getContext();
+
+
+		auto start = std::chrono::high_resolution_clock::now();
+
+		//Bufferek
+		cl::Buffer states(context, CL_MEM_READ_WRITE, byteSizeOfData);
 	
-	
 
-	auto start = std::chrono::high_resolution_clock::now();
-
-	//Bufferek
-	//cl::Buffer output(context, CL_MEM_WRITE_ONLY, N * N * sizeof(cl_double));
-	cl::Buffer states(context, CL_MEM_READ_WRITE, NN * N * N * sizeof(cl_double));
-	//cl::LocalSpaceArg state = cl::Local(N * N * sizeof(cl_double));
+		//Kernel argumentumok beállítása
+		_setArgs(ker, states, dataWidth, timeStep, stepsPerKernel);
+		_setArgs(ker2, states, dataWidth);
 
 
+		std::vector<cl::Event> prev;
+		cl::Event ev[workSize*workSize*globalSizeMultiplier*globalSizeMultiplier];
+
+		//Load the params
+		for (int x = 0; x < workersPerSide / 2; x++)
+			for (int y = 0; y < workersPerSide; y++)
+				queue.enqueueNDRangeKernel (ker2, cl::NDRange (x*workSize, y*workSize), cl::NDRange (workSize, workSize), cl::NDRange (workSize, workSize), NULL, &ev[x * workersPerSide + y]);
 
 
-	//Kernel argumentumok beállítása
-	_setArgs(ker, states, N, 1E-3, 50);
-	_setArgs(ker2, states, N);
+		for (int x = 0; x < workersPerSide / 2; x++)
+			for (int y = 0; y < workersPerSide; y++)
+				prev.push_back (ev[x * workersPerSide + y]);
 
-	//Load the params
-	cl::Event eventt;
-	queue.enqueueNDRangeKernel(ker2, 0, cl::NDRange(N, N), cl::NDRange(W, W), NULL, &eventt);
-
-	std::vector<cl::Event> prev({eventt});
-	for (int secs = 0; secs < 1000; secs++)
-	{
-		std::cout << secs << std::endl;
-
-		for (int i = 0; i < 20; i++)
+		for (int seconds = 0; seconds < simulationTime; seconds++) 
 		{
-			//Vége a számolásnak event
-			cl::Event ev[W];
-			//Számol
-			for (int j = 0; j < W; j++)
-				queue.enqueueNDRangeKernel(ker, cl::NDRange(j*W, 0), cl::NDRange(W, N), cl::NDRange(W, W), &prev, &ev[j]);
+			std::cout << seconds << std::endl;
 
-			//Következő feltételek vektorba
-			prev.clear();
-			for (int j = 0; j < W; j++)
-				prev.push_back(ev[j]);
-			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-		}
-		
-		//Írjuk ki az eredményt néha
-		if (secs % 10 == 0)
-		{
-			queue.enqueueReadBuffer(states, CL_TRUE, 0, NN * N * N * sizeof(cl_double), &states_vec[0], &prev);
-
-			std::ofstream file("temp.dat", std::ios::out);
-			for (int i = 0; i < N; i++)
+			for (int i = 0; i < CPUStepsPerSecond; i++)
 			{
-				for (int j = 0; j < N; j++)
-					for (int n = 0; n < NN; n++)
-						file << states_vec[(i*N + j)*NN + n] << "\t";
-				file << std::endl;
+				//Vége a számolásnak event
+				cl::Event ev[workSize*workSize*globalSizeMultiplier*globalSizeMultiplier];
+				//Számol
+				for (int x = 0; x < workersPerSide / 2; x++)
+					for (int y = 0; y < workersPerSide; y++)
+					queue.enqueueNDRangeKernel(ker, cl::NDRange(x*workSize, y*workSize), cl::NDRange(workSize, workSize), cl::NDRange(workSize, workSize), &prev, &ev[x * workersPerSide + y]);
+
+				//Következő feltételek vektorba
+				prev.clear();
+				for (int x = 0; x < workersPerSide / 2; x++)
+					for (int y = 0; y < workersPerSide; y++)
+						prev.push_back(ev[x * workersPerSide + y]);
+				std::this_thread::sleep_for(std::chrono::microseconds(10));
 			}
-
-			file.close();
 		}
-	}
-	//És kiolvas amint vége a számolásnak
-	auto ev_vec = prev;
-	queue.enqueueReadBuffer(states, CL_TRUE, 0, NN * N * N * sizeof(cl_double), &states_vec[0], &ev_vec);
+		//És kiolvas amint vége a számolásnak
+		auto ev_vec = prev;
+		queue.enqueueReadBuffer(states, CL_TRUE, 0, byteSizeOfData, &states_vec[0], &ev_vec);
 
-	//Nézzük meg az egész GPU-s dolog mennyi CPU oldali időbe telt (ne rondítsunk bele a std out streambe, errorba inkább...)
-	std::cerr << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << " us" << std::endl;
+		//Nézzük meg az egész GPU-s dolog mennyi CPU oldali időbe telt (ne rondítsunk bele a std out streambe, errorba inkább...)
+		std::cerr << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << " us" << std::endl;
 
-	std::ofstream file("out.dat", std::ios::out);
+		std::ofstream file("out.dat", std::ios::out);
 
-	//Írjuk ki az eredményt
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			file << states_vec[(i*N + j)*NN] << "\t";
+		//Írjuk ki az eredményt
+		for (int i = 0; i < dataWidth; i++) {
+			for (int j = 0; j < dataWidth; j++)
+				file << states_vec[(i*dataWidth + j) * numberOfStateVariables] << "\t";
+			file << std::endl;
 		}
-		file << std::endl;
-	}
 
-	file.close();
-
+		file.close();
 	}
 	//Ha valami gond lenne
 	catch(cl::Error err)
--- Pendulum/prog.cl
+++ Pendulum/prog.cl
@@ -3,10 +3,12 @@
 #define g 9.81
 #define PI 3.14159265358979323846264
 
-void derivs(__private double* x, __private double* f)
+typedef float number;
+
+void derivs(__private number* x, __private number* f)
 {
-	const double t1 = x[1], t2 = x[3], p1 = x[2], p2 = x[4];
-	const double COS = cos(t1 - t2);
+	const number t1 = x[1], t2 = x[3], p1 = x[2], p2 = x[4];
+	const number COS = cos(t1 - t2);
 
 	f[0] = 1;
 	f[1] = 6.0 / (1.0 * L * L) * (2 * p1 - 3 * COS * p2) / (16 - 9 * COS*COS);
@@ -16,20 +18,20 @@ void derivs(__private double* x, __private double* f)
 	f[4] = -1.0 / 2 * 1 * L* L * (-f[1] * f[3] * sin(t1 - t2) + g / L * sin(t2));
 }
 
-void addTo(__private double* to, __private double* what, double szorzo)
+void addTo(__private number* to, __private number* what, number szorzo)
 {
 	for (int i = 0; i < NN; i++)
 		to[i] += what[i] * szorzo;
 }
 
-void multiply(__private double* from, __private double* to, double szorzo)
+void multiply(__private number* from, __private number* to, number szorzo)
 {
 	for (int i = 0; i < NN; i++)
 		to[i] = from[i] * szorzo;
 }
 
 //  Fourth order Runge-Kutta
-void rk4(__private double* x, double tau, __private double* temp, __private double* deriv)
+void rk4(__private number* x, number tau, __private number* temp, __private number* deriv)
 {
 	derivs(x, deriv);
 
@@ -68,12 +70,10 @@ void rk4(__private double* x, double tau, __private double* temp, __private doub
 	//voila kész a lépés
 }
 
-__kernel void load(__global double* states, int N)
+__kernel void load(__global number* states, int N)
 {
 	const size_t g_x = get_global_id(0), g_y = get_global_id(1);
 	const size_t x = get_local_id(0), y = get_local_id(1);
-	const size_t g_w = get_local_size(0), g_h = get_local_size(1);
-	const size_t w = get_global_size(0), h = get_global_size(1);
 	
 	states[(g_y * N + g_x) * NN + 0] = 0;
 	states[(g_y * N + g_x) * NN + 1] = -3.0 + 6.0 * g_x / N;
@@ -82,14 +82,12 @@ __kernel void load(__global double* states, int N)
 	states[(g_y * N + g_x) * NN + 4] = 0;
 }
 
-__kernel void prog(__global double* states, int N, double dt, int steps)
+__kernel void prog(__global number* states, int N, number dt, int steps)
 {
 	const size_t g_x = get_global_id(0), g_y = get_global_id(1);
 	const size_t x = get_local_id(0), y = get_local_id(1);
-	const size_t g_w = get_local_size(0), g_h = get_local_size(1);
-	const size_t w = get_global_size(0), h = get_global_size(1);
 	
-	double state[NN], temp[NN], deriv[NN];
+	number state[NN], temp[NN], deriv[NN];
 
 	state[0] = states[(g_y * N + g_x) * NN + 0];
 	state[1] = states[(g_y * N + g_x) * NN + 1];
@@ -100,7 +98,7 @@ __kernel void prog(__global double* states, int N, double dt, int steps)
 
 	//Alapból -1
 	//times[g_y * N + g_x] = -1;
-			
+	
 	for (int i = 0; i < steps && !(state[3] < -PI || state[3] > PI); i++)
 	{
 		rk4(state, dt, temp, deriv);
