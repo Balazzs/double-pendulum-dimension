96d8ef848addcab2cf2169e1c0aca9a5f97869b7
Balazzs <bogye.balazs@gmail.com>
2019-10-22 20:52:17 +0200

    Common definitions moved to a header

---
+++ Pendulum/CommonDefinitions.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/cl.hpp>
+#else
+#include <CL/cl.hpp>
+#endif
+
+#include <functional>
+
+class Location;
+
+using number = cl_float;
+using CallbackFunction = std::function <void (const std::vector<std::pair<Location, number>>&)>;
+using Measurement = std::pair<Location, number>;
+
+constexpr int workSize = 32*32;
+constexpr int numberOfStateVariables = 5;
+
+constexpr int simulationTime = 10;
+constexpr int stepsPerSecond = 1024;
+constexpr int stepsPerKernel = 64;
+constexpr int CPUStepsPerSecond = stepsPerSecond / stepsPerKernel;
+constexpr number timeStep = 1.0 / stepsPerSecond;
+
+constexpr double paramWidth = 6.28, paramHeight = 6.28;
